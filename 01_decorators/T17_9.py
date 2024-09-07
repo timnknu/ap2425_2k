3b8de5fe92ddedf8196d1d1c777d48815ececccd
timn <i-wont-tell@you.com>
2024-09-07 09:36:08 +0300

    implementation without decorators

--- 01_decorators/T17_9.py
+++ 01_decorators/T17_9.py
@@ -9,8 +9,26 @@ max_N = 10
 storage = [None]*(max_N+1)
 
 def original_func(n):
+    print("Hi, I'm original function, with n =", n)
     res = n**2
     return res
 
 def cached_func(n):
-    #
+    if storage[n] is None:
+        res = original_func(n)
+        storage[n] = res
+        return res
+    else:
+        return storage[n]
+
+print('--------')
+print(cached_func(2))
+print('--------')
+print(cached_func(3))
+print('--------')
+print(cached_func(4))
+print('--------')
+print(cached_func(2))
+print('--------')
+print(cached_func(4))
+print('--------')

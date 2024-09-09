e3bbcd8f345fa7f78a06fdf448ebdfd1cd48b798
timn <i-wont-tell@you.com>
2024-09-09 08:32:27 +0300

    the starting point

---
+++ 01_decorators/T17_9_with_param.py
@@ -0,0 +1,29 @@
+
+max_N = 10
+def cacher(some_function):
+    storage = [None] * (max_N + 1)
+    def cached_func(n):
+        if storage[n] is None:
+            res = some_function(n)
+            storage[n] = res
+            return res
+        else:
+            return storage[n]
+    #
+    return cached_func
+
+
+@cacher  # original_func = cacher(original_func)
+def original_func(n):
+    print("Hi, I'm original function, with n =", n)
+    res = n**2
+    return res
+
+
+print('--------')
+print(original_func(2))
+print('--------')
+print(original_func(3))
+print('--------')
+print(original_func(3))
+print('--------')

6ccac6f2512eec6f057c092dd2ed401112644cd5
timn <i-wont-tell@you.com>
2024-09-09 08:59:13 +0300

    parameterization added

--- 01_decorators/T17_9_with_param.py
+++ 01_decorators/T17_9_with_param.py
@@ -1,19 +1,20 @@
-
-max_N = 10
-def cacher(some_function):
-    storage = [None] * (max_N + 1)
-    def cached_func(n):
-        if storage[n] is None:
-            res = some_function(n)
-            storage[n] = res
-            return res
-        else:
-            return storage[n]
+def Ncacher(max_N = 10):
+    def cacher(some_function):
+        storage = [None] * (max_N + 1)
+        def cached_func(n):
+            if storage[n] is None:
+                res = some_function(n)
+                storage[n] = res
+                return res
+            else:
+                return storage[n]
+        #
+        return cached_func
     #
-    return cached_func
+    return cacher
 
-
-@cacher  # original_func = cacher(original_func)
+@Ncacher(20)  # діє як original_func = cacher(original_func),
+# де cacher = Ncacher(10)
 def original_func(n):
     print("Hi, I'm original function, with n =", n)
     res = n**2

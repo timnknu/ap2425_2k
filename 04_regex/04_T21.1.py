5932a2bdd749ecd42d34b022423d8cc7a1d4032b
timn <i-wont-tell@you.com>
2024-10-21 10:11:05 +0300

    sub+groups demo for dates

--- 04_regex/04_T21.1.py
+++ 04_regex/04_T21.1.py
@@ -5,14 +5,18 @@ R_PATT_SL = r'(\d{4})/(\d{1,2})/(\d{1,2})'
 def myfunc(obj):
     print('THIS IS myfunc:', obj)
     print(obj.groups())
-    print(obj.group(1))
+    print()
     print(obj.group(2))
     print(obj.group(3))
     #'end', 'endpos', 'expand',
     # 'group', 'groupdict', 'groups', 'lastgroup',
     # 'lastindex', 'pos', 're', 'regs',
     # 'span', 'start', 'string'
-    return '++++++'
+    day = int(obj.group(3))
+    month = int(obj.group(2))
+    year = obj.group(1)
+    s = f"{day:02d}.{month:02d}.{year}"
+    return s
 
 with open('dates_inp1.txt', encoding='utf-8') as f:
     content = f.read()

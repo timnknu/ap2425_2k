5569ae70c996e89736d2bc0e58b922decf68b6f3
timn <i-wont-tell@you.com>
2024-10-21 10:04:55 +0300

    sub+groups demo

--- 04_regex/04_T21.1.py
+++ 04_regex/04_T21.1.py
@@ -1,9 +1,20 @@
 import re
 
-R_PATT_SL = r'\d{4}/\d{1,2}/\d{1,2}'
+R_PATT_SL = r'(\d{4})/(\d{1,2})/(\d{1,2})'
 
+def myfunc(obj):
+    print('THIS IS myfunc:', obj)
+    print(obj.groups())
+    print(obj.group(1))
+    print(obj.group(2))
+    print(obj.group(3))
+    #'end', 'endpos', 'expand',
+    # 'group', 'groupdict', 'groups', 'lastgroup',
+    # 'lastindex', 'pos', 're', 'regs',
+    # 'span', 'start', 'string'
+    return '++++++'
 
 with open('dates_inp1.txt', encoding='utf-8') as f:
     content = f.read()
-    s = re.sub(R_PATT_SL, '!!!!!!!', content)
+    s = re.sub(R_PATT_SL, myfunc, content)
     print(s)

f55fc0cd79dffef69b26dfd3f1a23126724861eb
timn <i-wont-tell@you.com>
2024-11-04 10:07:01 +0200

    argv demo

--- 05_OS/04_find_in_zip.py
+++ 05_OS/04_find_in_zip.py
@@ -1,4 +1,11 @@
 import zipfile
+import re
+
+P = "import\s.*"
+# Знайти файли, які містять тест,
+# що відповідає заданому регулярному виразу.
+#  Вивести імена знайдених файлів та
+#  знайдений текст
 
 with zipfile.ZipFile('../my-arch.zip') as zf:
     #print(zf.namelist())
@@ -6,7 +13,10 @@ with zipfile.ZipFile('../my-arch.zip') as zf:
         with zf.open(item) as cf:
             content = cf.read()
             s = content.decode('utf-8')
-            print('-'*80)
-            print(item.filename)
-            print(s)
-            print('-'*80)
+            fnd = re.findall(P, s)
+            if len(fnd)>0:
+                print('Found in', item.filename)
+                for substr in fnd:
+                    print(substr)
+                print('----')
+

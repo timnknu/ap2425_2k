1042fb657916ffef1a79d0e2778d2cf23b8a736d
Your Name <you@example.com>
2025-02-03 13:30:21 +0200

    client and server work in ideal conditions

--- 09_sockets_and_threading/char-srv-v2.py
+++ 09_sockets_and_threading/char-srv-v2.py
@@ -7,14 +7,20 @@ PORT = 5555  # Порт для з'єднання
 
 def work_with_client(conn, address):
     print('New thread created for', address)
+    user_nickname = None
     try:
-        data = conn.recv(1024)
-        s = str(data, encoding='utf-8')
-        print('I received:', s, 'from', address)
         while True:
-            s = str(time.time()) + '\n'
-            conn.sendall(s.encode())
-            time.sleep(0.2)
+            data = conn.recv(1024)
+            s = str(data, encoding='utf-8')
+            print('I received:', s, 'from', address)
+            if user_nickname is None:
+                user_nickname = s
+                for cs in client_sockets:
+                    cs.sendall( bytes(f'Hey! We have a new participant: {user_nickname} from {address}', encoding='utf-8') )
+            else:
+                s = user_nickname + ' --> ' + s
+                for cs in client_sockets:
+                    cs.sendall( bytes(s, encoding='utf-8') )
     except socket.error as e:
         print(e)
 
@@ -27,9 +33,12 @@ try:
     srv.listen(5)  # очікувати на з'єднання до 5 клієнтів одночасно
     print('=== Chat server ===')
 
+    client_sockets = []
+
     while True:
         print('** waiting for connection **')
         conn, address = srv.accept()
+        client_sockets.append(conn)
         th = threading.Thread(target=work_with_client, args=(conn, address))
         th.start()
 

f066a1eba4af97a6960713b4cbe49058861cec4e
Your Name <you@example.com>
2025-02-03 12:51:58 +0200

    initial template for chat client GUI

--- 09_sockets_and_threading/tk-chat-client.py
+++ 09_sockets_and_threading/tk-chat-client.py
@@ -1,50 +1,30 @@
 import tkinter as tk
-import tkinter.font as tkFont
 
-class App:
-    def __init__(self, root):
-        #setting title
-        root.title("undefined")
-        #setting window size
-        width=600
-        height=500
-        screenwidth = root.winfo_screenwidth()
-        screenheight = root.winfo_screenheight()
-        alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
-        root.geometry(alignstr)
-        root.resizable(width=False, height=False)
+root = tk.Tk()
 
-        GLineEdit_370=tk.Entry(root)
-        GLineEdit_370["borderwidth"] = "1px"
-        ft = tkFont.Font(family='Times',size=10)
-        GLineEdit_370["font"] = ft
-        GLineEdit_370["fg"] = "#333333"
-        GLineEdit_370["justify"] = "center"
-        GLineEdit_370["text"] = "Entry"
-        GLineEdit_370.place(x=30,y=350,width=490,height=43)
+root.title("chat client")
+width=600
+height=500
+screenwidth = root.winfo_screenwidth()
+screenheight = root.winfo_screenheight()
+alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
+root.geometry(alignstr)
+root.resizable(width=False, height=False)
 
-        GButton_952=tk.Button(root)
-        GButton_952["bg"] = "#e9e9ed"
-        ft = tkFont.Font(family='Times',size=10)
-        GButton_952["font"] = ft
-        GButton_952["fg"] = "#000000"
-        GButton_952["justify"] = "center"
-        GButton_952["text"] = "Button"
-        GButton_952.place(x=530,y=360,width=70,height=25)
-        GButton_952["command"] = self.GButton_952_command
+myMessasaEntry=tk.Entry(root)
+myMessasaEntry.place(x=30,y=350,width=490,height=43)
 
-        GListBox_318=tk.Listbox(root)
-        GListBox_318["borderwidth"] = "1px"
-        ft = tkFont.Font(family='Times',size=10)
-        GListBox_318["font"] = ft
-        GListBox_318["fg"] = "#333333"
-        GListBox_318["justify"] = "center"
-        GListBox_318.place(x=30,y=30,width=499,height=298)
+sendMyMessageButton=tk.Button(root)
+sendMyMessageButton["text"] = "Send"
+sendMyMessageButton.place(x=530,y=360,width=70,height=25)
 
-    def GButton_952_command(self):
-        print("command")
+msgsFromOthers=tk.Listbox(root)
+msgsFromOthers.place(x=30,y=30,width=499,height=298)
 
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = App(root)
-    root.mainloop()
+def send_msg_to_server():
+    print("send")
+
+sendMyMessageButton["command"] = send_msg_to_server
+
+
+root.mainloop()

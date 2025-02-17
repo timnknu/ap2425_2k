794a5a342ba8b4d2462ee196c85e518adaef99b8
Your Name <you@example.com>
2025-02-17 13:38:46 +0200

    minimal working version

--- 11_web_and_html_2/mini-server-cls.py
+++ 11_web_and_html_2/mini-server-cls.py
@@ -5,21 +5,32 @@ import socketserver
 class RequestHandler(socketserver.StreamRequestHandler):
     def handle(self):
         print('connected from', self.client_address)
+        request_lines = []
         while True:
-            data = self.rfile.readline().strip()
-            pal = str(data, encoding = 'utf-8')
-            res = bytes(self._test_palindrome(pal)) + b'\n'
-            self.wfile.write(res)
-        print('disconnected', self.client_address)
+            data = self.rfile.readline()
+            line = str(data, encoding = 'utf-8')
+            if len(line.rstrip()) == 0: # перевірити, чи складається рядок тільки з символів кінця рядка (\r, \n)
+                break
+            request_lines.append(line)
+        #
+        print('Request lines', request_lines)
+
+        resp_str = "HTTP/1.1 200 OK\n"
+        resp_str = resp_str + "Content-Type: text/html\n"
+        resp_str = resp_str + "\n"
+        resp_str = resp_str + "Hello, world!\n\n"
+        resp_str = resp_str + "the first line of request was: " + request_lines[0] + "\n"
+        #self.wfile.write(resp_str.encode('utf-8'))
+        self.wfile.write(bytes(resp_str, encoding='utf-8'))
 
 #--------------------------------------------------------------------
 # Блок 3
 HOST = ''                 # Комп'ютер для з'єднання
-PORT = 5556              # Порт для з'єднання
+PORT = 5557              # Порт для з'єднання
 
 srv = socketserver.ThreadingTCPServer((HOST, PORT), RequestHandler)
-socketserver.TCPServer.allow_reuse_address = True
-socketserver.TCPServer.allow_reuse_port = True
+socketserver.ThreadingTCPServer.allow_reuse_address = True
+socketserver.ThreadingTCPServer.allow_reuse_port = True
 
 try:
     srv.serve_forever()

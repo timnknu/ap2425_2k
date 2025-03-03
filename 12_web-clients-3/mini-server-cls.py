81c5abcbd20bb8082bc920e1496f9746193016a7
Your Name <you@example.com>
2025-03-03 13:58:30 +0200

    minimal GET parsing

--- 12_web-clients-3/mini-server-cls.py
+++ 12_web-clients-3/mini-server-cls.py
@@ -1,4 +1,6 @@
 import socketserver
+import urllib.parse
+import math
 
 #--------------------------------------------------------------------
 # Блок 2
@@ -20,12 +22,26 @@ class RequestHandler(socketserver.StreamRequestHandler):
         resp_str = resp_str + "Content-Type: text/html\n"
         resp_str = resp_str + "Content-encoding: utf-8\n"
         resp_str = resp_str + "\n"
-        if pth == '/':
+
+        uri_parts = urllib.parse.urlparse(pth)
+
+        if uri_parts.path == '/':
             with open('my-page.html', 'r') as pgfile:
                 content = pgfile.read()
             resp_str = resp_str + content
-        elif pth == '/about':
+        elif uri_parts.path == '/about':
             resp_str = resp_str + "This is a second piece of information, and it is not an HTML\n\n"
+        elif uri_parts.path == '/myprog':
+            d = urllib.parse.parse_qs(uri_parts.query)
+            #resp_str = resp_str + f"you entered: {d['var']}\n\n"
+            try:
+                list_of_vals = d['var']
+                x = float(list_of_vals[0])
+                y = math.sin(x)
+                resp_str = resp_str + f"Значення sin: {y}\n\n"
+            except:
+                resp_str = resp_str + "Ой, щось ви не те ввели...\n\n"
+
         #
         self.wfile.write(bytes(resp_str, encoding='utf-8')) # перетворити рядок у байти та відправити клієнту (або можна було так: self.wfile.write(resp_str.encode('utf-8')) )
 

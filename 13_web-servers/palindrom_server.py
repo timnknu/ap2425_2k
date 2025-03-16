a97164db549d53f6bf8a18dae48722abf52099b7
Your Name <you@example.com>
2025-03-16 23:48:53 +0200

    blocks numbering changed

--- 13_web-servers/palindrom_server.py
+++ 13_web-servers/palindrom_server.py
@@ -1,10 +1,10 @@
 import http.server
 
-# Блок 2
+# Блок 1
 RequestHandler = http.server.CGIHTTPRequestHandler
 
 #--------------------------------------------------------------------
-# Блок 3
+# Блок 2
 HOST = ''                 # Комп'ютер для з'єднання
 PORT = 5556              # Порт для з'єднання
 
@@ -12,6 +12,7 @@ http.server.ThreadingHTTPServer.allow_reuse_address = True
 http.server.ThreadingHTTPServer.allow_reuse_port = True
 srv = http.server.ThreadingHTTPServer((HOST, PORT), RequestHandler)
 
+# Блок 3
 try:
     srv.serve_forever()
 except:

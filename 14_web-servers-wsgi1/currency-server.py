8f1414df905e7163b15c4a8cfc1b4ba5853e6daf
timn <i-wont-tell@you.com>
2025-03-17 12:47:55 +0200

    data read as func

--- 14_web-servers-wsgi1/currency-server.py
+++ 14_web-servers-wsgi1/currency-server.py
@@ -1,6 +1,14 @@
 import openpyxl
 
-wb = openpyxl.load_workbook('rates-example.xlsx')
-ws = wb.worksheets[0]
-for r in ws.iter_rows(values_only=True):
-    print(r)
+def read_rates():
+    rates = {}
+    wb = openpyxl.load_workbook('rates-example.xlsx')
+    ws = wb.worksheets[0]
+    for r in ws.iter_rows(values_only=True):
+        ccode1, ccode2, rate = r
+        key = (ccode1, ccode2)
+        rates[key] = rate
+    return rates
+#-----------------------------------------
+
+

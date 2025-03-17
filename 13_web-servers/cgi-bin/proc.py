c69912e33244cec9719e2e2f198a0fa91d827c6d
timn <i-wont-tell@you.com>
2025-03-17 12:17:13 +0200

    palindrom check in proc.py

--- 13_web-servers/cgi-bin/proc.py
+++ 13_web-servers/cgi-bin/proc.py
@@ -1,8 +1,7 @@
 import os
 import urllib.parse
 
-#print('Content-type: text/plain')
-print('Content-type: text/html')
+print('Content-type: text/html')   # або 'text/plain'
 print()
 print('<!DOCTYPE html>')
 print('<html>')
@@ -10,13 +9,16 @@ print('<head>')
 print('<meta charset="utf8">')
 print('</head>')
 print('<body>')
-print('Hello world')
-for i in range(10):
-    print(i, i**2, '<br>')
-print('<hr>')
 
 req = os.environ['QUERY_STRING']
 form_data = urllib.parse.parse_qs(req)
-print(form_data)
+values_list = form_data['inp1']
+#print('Ви ввели:', values_list[0])
+txt = values_list[0]
+if txt == txt[::-1]:
+    print(f'Це паліндром: <pre>{txt}</pre>')
+else:
+    print(f'Це <b>не</b> є паліндромом: <pre>{txt}</pre>')
+
 print('</body>')
 print('</html>')

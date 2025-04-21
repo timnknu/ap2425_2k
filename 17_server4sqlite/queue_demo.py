ff1e36d3ba258dfacec942f4f9038d1eab2096a6
timn <i-wont-tell@you.com>
2025-04-21 11:20:14 +0300

    bidirectional queue example added

--- 17_server4sqlite/queue_demo.py
+++ 17_server4sqlite/queue_demo.py
@@ -3,11 +3,14 @@ import queue
 import time
 
 q = queue.Queue()
+qback = queue.Queue()
 
 def prog_logic():
     print('Thread started!')
     x = q.get()
     print('We received', x)
+    time.sleep(2)
+    qback.put(x**2)
     print('Thread finished')
 
 
@@ -17,7 +20,7 @@ def main():
     th.start()
     time.sleep(2)
     q.put(12)
-    time.sleep(1)
+    print('Result:', qback.get())
     print('Main program finished')
 
 if __name__ == "__main__":

7eb1d8e2416d81557f2a1369d8dde92851df9981
timn <i-wont-tell@you.com>
2025-04-28 13:00:29 +0300

    doctest demo

---
+++ 18_testing/t30_3.py
@@ -0,0 +1,22 @@
+import doctest
+
+def Hamming_dist(s1, s2):
+    """
+    Відстанню Хемінга між s1 та s2 називають кількість позицій, у
+    яких s1[i] != s2[i]
+    >>> Hamming_dist('hello', 'world')
+    4
+
+    >>> Hamming_dist('hello', 'world123')
+    7
+
+    """
+    num_different = 0
+    for c1, c2 in zip(s1, s2):
+        if c1!=c2:
+            num_different += 1
+    num_different += abs(len(s1) - len(s2))
+    return num_different
+
+if __name__=="__main__":
+    doctest.testmod(verbose=2)
